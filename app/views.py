106b03945795a18be3f15c8db074411b35a428cb
Mowzli Sre <mowzlisre@Mowzlis-Mac-mini.local>
2021-04-30 16:20:17 +0530

    New changes

--- app/views.py
+++ app/views.py
@@ -41,6 +41,10 @@ class ProfileEditView(LoginRequiredMixin, View):
         profile.address = request.POST.get('address')
         profile.city = request.POST.get('city')
         profile.profilePicture = request.POST.get('profilePicture')
+        profile.license = request.POST.get('license')
+        profile.panCard = request.POST.get('panCard')
+        profile.vehicleType = request.POST.get('vehicleType')
+        profile.aadhar = request.POST.get('aadhar')
         profile.save()
         messages.success(request, 'Profile has been updated!')
         return redirect('profile')
@@ -75,14 +79,23 @@ class VolunteerAction(View):
     def get(self, request, id):
         activity = Activity.objects.get(id=id)
         if request.user.is_authenticated:
-            activity.volunteer = request.user.username
-            timeLine = TimeLine()
-            timeLine.user = request.user.username
-            timeLine.type = 'Volunteer'
-            timeLine.description = 'Volunteered at %s for a %s %s in the condition of %s' %(activity.location, activity.breed, activity.pet, activity.condition)
-            timeLine.save()
-            activity.actionStamp = timezone.now()
-            activity.save()
+            user = Profile.objects.get(user__username=request.user.username)
+            print(user.license, user.aadhar)
+            if user.license != '' and user.aadhar != '':
+                if not user.isVolunteer:
+                    user.isVolunteer = True
+                    user.save()
+                activity.volunteer = user.user.username
+                timeLine = TimeLine()
+                timeLine.user = user.user.username
+                timeLine.type = 'Volunteer'
+                timeLine.description = 'Volunteered at %s for a %s %s in the condition of %s' %(activity.location, activity.breed, activity.pet, activity.condition)
+                timeLine.save()
+                activity.actionStamp = timezone.now()
+                activity.save()
+                messages.success(request, 'You are now volunteering the activity!')
+            else:
+                messages.success(request, 'Please update your profile with Aadhar and License Details')
         return redirect('volunteer-home')
 
 class NewActivity(View):
@@ -110,18 +123,22 @@ class AdoptView(View):
 
 class AdoptPetView(View):
     def get(self, request, id):
-        pet = PetProfile.objects.get(id=id)
-        pet.adopted = True
-        pet.save()
-        timeLine = TimeLine()
-        timeLine.user = request.user.username
-        timeLine.type = 'Adopte'
-        timeLine.description = 'Adopted %s, a %s %s' %(pet.name, pet.sex, pet.breed)
-        timeLine.save()
-        profile = Profile.objects.get(user__username=timeLine.user)
-        profile.isAdopter = True
-        profile.save()
-        messages.success(request, 'You have successfully adopted %s' %(pet.name))
+        user = Profile.objects.get(user__username=request.user.username)
+        if user.aadhar != '':
+            if not user.isAdopter:
+                user.isAdopter = True
+                user.save()
+            pet = PetProfile.objects.get(id=id)
+            pet.adopted = True
+            pet.save()
+            timeLine = TimeLine()
+            timeLine.user = request.user.username
+            timeLine.type = 'Adopte'
+            timeLine.description = 'Adopted %s, a %s %s' %(pet.name, pet.sex, pet.breed)
+            timeLine.save()
+            messages.success(request, 'You have successfully adopted %s' %(pet.name))
+        else:
+            messages.success(request, 'Please update your profile with Aadhar Details')
         return redirect('adoption-page')
 
 class DonatePaymentPageView(View):
@@ -129,14 +146,21 @@ class DonatePaymentPageView(View):
         return render(request, 'donate/home.html')
     
     def post(self, request):
-        profile = Profile.objects.get(user__username=request.user.username)
-        profile.donatedAmount += float(request.POST.get('amount'))
-        profile.isDonor = True
-        profile.save()
-        timeLine = TimeLine()
-        timeLine.user = request.user.username
-        timeLine.type = 'Donate'
-        timeLine.description = 'Donated ₹ %s' %(float(request.POST.get('amount')))
-        timeLine.save()
-        messages.success(request, 'Donation successful')
+        if request.POST.get('otp') == '123456':
+            profile = Profile.objects.get(user__username=request.user.username)
+            if profile.aadhar != '' and profile.panCard != '':
+                profile.donatedAmount += float(request.POST.get('amount'))
+                if not profile.isDonor:
+                    profile.isDonor = True
+                profile.save()
+                timeLine = TimeLine()
+                timeLine.user = request.user.username
+                timeLine.type = 'Donate'
+                timeLine.description = 'Donated ₹ %s' %(float(request.POST.get('amount')))
+                timeLine.save()
+                messages.success(request, 'Donation successful')
+            else:
+                messages.success(request, 'Please your profile with Aadhar and Pan Card Details')
+        else:
+            messages.success(request, 'Payment Failed')
         return redirect('profile')
